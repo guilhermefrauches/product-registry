581e65ca2282e647319729a7eae8e2101c98a3cb
Guilherme-Dev-21 <gmoreira2610@gmail.com>
2025-10-22 17:24:58 -0300

    feat: implement repositories for Category and Product

---
+++ src/main/java/br/com/product/registry/repository/CategoryCollectionRepository.java
@@ -0,0 +1,57 @@
+package br.com.product.registry.repository;
+
+import br.com.product.registry.model.Category;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Vector;
+
+public class CategoryCollectionRepository {
+
+    private static List<Category> categoryList;
+
+    static {
+
+        categoryList = new Vector<>();
+
+        Category electronics = new Category("Electronics", 1l);
+        Category CellPhones = new Category("Cell Phones", 2l);
+        Category Books = new Category("Books", 3l);
+
+
+        categoryList.add(electronics);
+        categoryList.add(CellPhones);
+        categoryList.add(Books);
+
+    }
+
+    public static List<Category> findAll() {
+        return categoryList;
+    }
+
+    public static Category findById(Long id) {
+        return categoryList.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    public static List<Category> findByName(String name) {
+        return categoryList.stream()
+                .filter(c -> c.getName().equalsIgnoreCase(name))
+                .toList();
+    }
+
+    public static Category save(Category category) {
+        if (!categoryList.contains(category)) {
+            category.setId((long) categoryList.size() + 1);
+            categoryList.add(category);
+            return category;
+        } else {
+            JOptionPane.showMessageDialog(null, "Category already exists!");
+        }
+        return category;
+    }
+
+}
---
+++ src/main/java/br/com/product/registry/repository/ProductsCollectionRepository.java
@@ -0,0 +1,44 @@
+package br.com.product.registry.repository;
+
+import br.com.product.registry.model.Product;
+
+import javax.swing.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import java.util.List;
+import java.util.Vector;
+
+
+public class ProductsCollectionRepository {
+
+    private static List <Product>  productList;
+
+    static  {
+
+        productList = new Vector<Product>();
+
+//        Product cell = new Product();
+//
+//        cell.setName("Iphone 14 Pro Max")
+//                .setDescription("Cell Phone of the apple")
+//                .setCategory(CategoryCollectionRepository.findById(2L))
+//                .setCreationDate(LocalDateTime.now())
+//                .setPrice(BigDecimal.valueOf(12000));
+//
+//        save(cell);
+
+    }
+
+    public static Product save(Product product) {
+        if (!productList.contains(product)) {
+            product.setId((long) productList.size() + 1);
+            productList.add(product);
+            return product;
+        } else  {
+            JOptionPane.showMessageDialog(null, "Product already exists!");
+            return null;
+        }
+
+    }
+}
